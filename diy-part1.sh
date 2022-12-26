0674c9f63cad1c38580cfe72f659207ec2505f4d
surfboy2000 <52144375+surfboy2000@users.noreply.github.com>
2022-12-26 21:15:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,6 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.de
 
 # ADD my luci-theme
 rm -rf package/lean/luci-theme-argon
-git clone  https://github.com/surfboy2000/luci-theme-argon.git package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/surfboy2000/luci-theme-argon.git package/lean/luci-theme-argon
 
 
