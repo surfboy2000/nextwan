ae698b87ba3ca169ada1eda9e8649741b7f9ef0a
surfboy2000 <52144375+surfboy2000@users.noreply.github.com>
2022-12-26 14:53:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.89.1/g' package/base-files/files/bin/config_generate
+
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+
